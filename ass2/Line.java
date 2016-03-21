193c32912def948e8fdd39922afdc0f45c60ea04
orie50 <turht50@gmail.com>
2016-03-21 10:15:33 +0200

    Added files via upload

--- ass2/Line.java
+++ ass2/Line.java
@@ -3,16 +3,18 @@ package ass2;
 public class Line {
     private Point start;
     private Point end;
-    private Point middle;
+    private double slope;
        // constructors
         public Line(Point start, Point end) {
            this.start = start;
            this.end = end;
-	   }
-	   public Line(double x1, double y1, double x2, double y2) {
+           this.slope = slope();
+           }
+       public Line(double x1, double y1, double x2, double y2) {
 		   this.start = new Point(x1,y1);
 		   this.end = new Point(x2,y2);
-		   this.middle = new Point((x2+x1)/2,(y2+y1)/2);
+		   
+		   this.slope = slope();
 	   }
 
 	   // Return the length of the line
@@ -23,8 +25,14 @@ public class Line {
 
 	   // Returns the middle point of the line
 	public Point middle() {
-		return middle;
-	   }
+		double x1, x2 ,y1 ,y2;
+		x1 = this.start.getX();
+		x2 = this.end.getX();
+		y1 = this.start.getY();
+		y2 = this.end.getY();
+		Point middle = new Point((x2+x1)/2,(y2+y1)/2);
+		return middle; 
+	}
 
 	   // Returns the start point of the line
 	   public Point start() {
@@ -36,6 +44,12 @@ public class Line {
 		   return end;
 	   }
 
+	   public double slope(){
+		   double dx, dy;
+		   dx = this.start.getX() - this.end.getX();
+		   dy = this.start.getY() - this.end.getY();
+		   return (dy / dx);
+	   }
 	   // Returns true if the lines intersect, false otherwise
 	   public boolean isIntersecting(Line other) {
 		   Point intersection = intersectionWith(other);
@@ -49,30 +63,76 @@ public class Line {
 
 	   // Returns the intersection point if the lines intersect,
 	   // and null otherwise.
-	   public Point intersectionWith(Line other) {
-			   double m1, m2, X, Y;
-			   m1 = (this.start.getY() - this.end.getY()) / (this.start.getX() - this.end.getX());
-			   m2 = (other.start.getY() - other.end.getY()) / (other.start.getX() - other.end.getX());
-			   X = (other.start.getY() - this.start.getY() - m2 * other.start.getX() + m1 * this.start.getX()) / (m1-m2);
-			   Y = m1*X + this.start.getY()- m1 * this.start.getX();
-			   Point intersection = new Point(X,Y);
-		if (intersection.getX()< this.start.getX() && intersection.getX() > this.end.getX() || intersection.getX() > this.start.getX() && intersection.getX() < this.end.getX()){
+        public Point intersectionWith(Line other) {
+			   double x, y, m1, m2, b1, b2;
+			   m1 = this.getSlope();
+			   m2 = other.getSlope();
+			   b1 = this.start.getY() - m1 * this.start.getX();
+			   b2 = other.start.getY() - m2 * other.start.getX();
+			   
+			   if (Double.isInfinite(m1)) {
+					   x = this.start.getX();
+					   y = m2 * x + b2;
+					   if(!IsInTheDomain(y, this.start.getY(), this.end.getY())){
+						   return null;
+				   }
+				}
+			   else if (Double.isInfinite(m2)) {
+					   x = other.start.getX();
+					   y = m1 * x + b1;
+					   if(!IsInTheDomain(y, other.start.getY(), other.end.getY())){
+						   return null;
+					   }
+				}
+			   else if(m1 == m2){
+			      if ((this.start.equals(other.start)) || (this.start.equals(other.end))){
+			    	  y = this.start.getY();
+			    	  x = this.start.getX();
+			      }
+			      else if (this.end.equals(other.start) || (this.end.equals(other.end))){
+			    	  y = this.end.getY();
+			    	  x = this.end.getX();
+			   }
+			      else{
+			    	  return null;
+			      }
+			   }
+			   
+			   else{
+               x = (b2 - b1) / (m1 - m2);
+               y = m1 * x + b1;
+               if (!(x > this.start.getX() && x < this.end.getX() || x < this.start.getX() && x > this.end.getX())){
+            	   return null;
+			       }
+               }
+			Point intersection = new Point(x, y);
 			return intersection;
-		}
-		   else{
-		   return null;
-	   }
 	}
+
+	   public boolean IsItEqual(double y, double y2) {
+			if (y == y2){
+				return true;
+			}
+			return false;
+		}
+	boolean IsInTheDomain(double x1, double x2Start, double x2End) {
+		   if ((x1 > x2Start && x1 < x2End) || (x1 < x2Start && x1 > x2End)) {
+			   return true;
+		   }
+			   return false;
+	   }
 
 	   // equals -- return true is the lines are equal, false otherwise
 	   public boolean equals(Line other) {
-		   if (other == null){
+		   if (other == null) {
 			   return false;
 		   }
-		   if (this.length() == other.length()){
+		   if (this.start.equals(other.start) && this.end.equals(other.end)) {
 			   return true;
 		   }
 		   return false;
 	   }
-
+	public double getSlope() {
+		return slope;
+	}
 	}
--- ass2/Point.java
+++ ass2/Point.java
@@ -1,41 +1,51 @@
 package ass2;
-
+/**
+* @author Ori Engelberg <turht50@gmail.com>
+* @version 1.0
+* @since 2016-03-02 */
 public class Point {
 
-	private double x;
-	private double y;
+    private double x;
+    private double y;
 
-	public Point(double x, double y) {
-		
-		this.x = x;
-		this.y = y;
+    public Point(double x, double y) {
+
+        this.x = x;
+        this.y = y;
     }
-	
-	public double distance(Point other) {
-		if (other == null){
-			return 0;
-		}
-        return Math.sqrt(((this.x - other.x) *(this.x - other.x)) + ((this.y - other.y)*(this.y - other.y)));
-	}
-	
-	public boolean equals(Point other) { 
-		if (other == null){
-			return false;
-		}
-		double distance = distance(other);
-		if (distance == 0){
-		return true;
-		}
-		else{
-			return false;
-		}
-		
-	}
-	
-	 public double getX() {
-	      return this.x;
-	   }
-	 public double getY() {
-	      return this.y;
-	   }
+
+    public double distance(Point other) {
+        if (other == null){
+           return 0;
+        }
+        double x1, x2, y1, y2;
+        x1 = this.getX();
+        x2 = other.getX();
+        y1 = this.getY();
+        y2 = other.getY();
+        return Math.sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
+    }
+
+    public boolean equals(Point other) { 
+    	if (other == null){
+            return false;
+        }
+        double x1, x2, y1, y2;
+        x1 = this.getX();
+        x2 = other.getX();
+        y1 = this.getY();
+        y2 = other.getY();
+        if (x1 == x2 && y1 == y2){
+        return true;
+        }
+            return false;
+
+    }
+
+    public double getX() {
+        return this.x;
+        }
+    public double getY() {
+        return this.y;
+        }
 }
