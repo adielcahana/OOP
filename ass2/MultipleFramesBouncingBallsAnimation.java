c0b32f178d33e4de1f1b98cd2e1f336d23961c9f
adielcahana <adielcahana@users.noreply.github.com>
2016-03-27 13:47:24 +0300

    Added files via upload

--- ass2/MultipleFramesBouncingBallsAnimation.java
+++ ass2/MultipleFramesBouncingBallsAnimation.java
@@ -1,44 +1,46 @@
 package ass2;
 
 import java.awt.Color;
-import java.util.Random;
 import biuoop.DrawSurface;
 import biuoop.GUI;
 import biuoop.Sleeper;
-
+/**
+* @author Adiel cahana <adiel.cahana@gmail.com>
+* @version 1.0
+* @since 2016-03-25 */
 public class MultipleFramesBouncingBallsAnimation {
-
-	public static void main(String[] args) {
-		System.out.println(args[0]);
-		Sleeper sleeper = new Sleeper();
-		int[] sizes = Sort.bubbleSort(args);
-		Random rand = new Random();
-		Ball[] balls = new Ball[args.length];
-		GUI gui = new GUI("Multple frames", 850, 850);
-		for(int i = 0; i < args.length/2; i++){
-            balls[i]= new Ball(51 + rand.nextInt(450),51 +rand.nextInt(450),sizes[i]
-            		           ,Color.BLACK, new Point(50,50), new Point(500,500));
-			balls[i].setVelocity(args.length - i + 1, args.length - i +1);
-		}
-		for(int i = args.length/2 ; i < args.length; i++){
-            balls[i]= new Ball(451 + rand.nextInt(150), 451 + rand.nextInt(150), sizes[i] ,
-            		           Color.BLACK, new Point(450,450), new Point(600,600));
-			balls[i].setVelocity(args.length - i + 1, args.length - i + 1);
-		}
-		
-		while (true) {
-			DrawSurface d = gui.getDrawSurface();
-			d.setColor(Color.GRAY);
-			d.drawRectangle(50, 50, 450, 450);
-			d.setColor(Color.YELLOW);
-			d.drawRectangle(450, 450, 150, 150);
-			for(int i = 0; i < args.length; i++){
-			   balls[i].moveOneStep();
-			   balls[i].drawOn(d);
-			}
-		    gui.show(d);
-		    sleeper.sleepFor(50);  // wait for 50 milliseconds.
-		    }
-	}
-
+    /**
+     * draw 2 frames with balls.
+     * <p>
+     * @param args - sizes for the balls. */
+    public static void main(String[] args) {
+        //create an array for half of the sizes
+        String[] sizes = new String[args.length / 2];
+        for (int i = 0; i < sizes.length; i++) {
+            sizes[i] = args[i];
+        }
+        Ball[] balls1 = MultipleBouncingBallsAnimation.getBallsArray(sizes, new Point(50, 50),
+                                                                     new Point(500, 500));
+        //create an array for the other half of the sizes
+        sizes = new String[args.length - args.length / 2];
+        for (int i = 0; i < sizes.length; i++) {
+            sizes[i] = args[i + args.length / 2];
+        }
+        Ball[] balls2 = MultipleBouncingBallsAnimation.getBallsArray(sizes, new Point(450, 450),
+                                                                     new Point(600, 600));
+        Sleeper sleeper = new Sleeper();
+        GUI gui = new GUI("Multiple frames", 610, 610);
+        while (true) {
+            DrawSurface surface = gui.getDrawSurface();
+            //draw 2 frames
+            surface.setColor(Color.GRAY);
+            surface.drawRectangle(50, 50, 450, 450);
+            surface.setColor(Color.YELLOW);
+            surface.drawRectangle(450, 450, 150, 150);
+            MultipleBouncingBallsAnimation.drawBallArray(balls1, surface);
+            MultipleBouncingBallsAnimation.drawBallArray(balls2, surface);
+            gui.show(surface);
+            sleeper.sleepFor(50);  // wait for 50 milliseconds.
+            }
+    }
 }
