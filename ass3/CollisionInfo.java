c442a72fd3adb0861d749ba9d631b2ae5f5cc3b5
adielcahana <adielcahana@users.noreply.github.com>
2016-04-07 01:45:04 +0300

    Added files via upload

--- ass3/CollisionInfo.java
+++ ass3/CollisionInfo.java
@@ -1,17 +1,30 @@
+/**
+* @author Adiel cahana <adiel.cahana@gmail.com>
+* @version 1.0
+* @since 2016-04-02 */
 public class CollisionInfo {
     private Point collisionPoint;
     private Collidable collisionObject;
-
+    /**
+     * CollisionInfo constructor.
+     * <p>
+     * @param collisionPoint - the next collision coordinate
+     * @param collisionObject - the object that we collided with*/
     public CollisionInfo(Point collisionPoint, Collidable collisionObject) {
         this.collisionObject = collisionObject;
         this.collisionPoint = collisionPoint;
     }
-    // the point at which the collision occurs.
+    /**
+     * Collision point getter.
+     * <p>
+     * @return collisionPoint - the point at which the collision occurs.*/
     public Point collisionPoint() {
         return this.collisionPoint;
     }
-
-    // the collidable object involved in the collision.
+    /**
+     * Collision object getter.
+     * <p>
+     * @return collisionObject - the collidable object involved in the collision*/
     public Collidable collisionObject() {
         return this.collisionObject;
     }
