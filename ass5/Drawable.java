803b95e232ca134fea5721d4523b8d7ba46b9e25
adielcahana <adielcahana@users.noreply.github.com>
2016-05-23 19:34:08 +0300

    Add files via upload

---
+++ ass5/Drawable.java
@@ -0,0 +1,9 @@
+import biuoop.DrawSurface;
+
+public interface Drawable {
+    /**
+     * DrawOn the drawable.
+     * <p>
+     * @param sprite - the surface of the sprite to draw.    */
+   void drawOn(DrawSurface ds);
+}
