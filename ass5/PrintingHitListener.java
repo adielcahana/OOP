f0b75d764cf7cecdad6a4ff913bf9de488977655
orie50 <turht50@gmail.com>
2016-05-28 22:26:53 +0300

    Delete PrintingHitListener.java

--- ass5/PrintingHitListener.java
+++
@@ -1,6 +0,0 @@
-
-public class PrintingHitListener implements HitListener {
-   public void hitEvent(Block beingHit, Ball hitter) {
-      System.out.println("A Block with " + beingHit.getMaxHits() + " points was hit.");
-   }
-}
