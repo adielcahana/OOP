ab2edd52d7ac4c3874561df0d18a91744139af57
orie50 <turht50@gmail.com>
2016-06-09 13:42:10 +0300

    Add files via upload

--- ass6/general/ColorsParser.java
+++ ass6/general/ColorsParser.java
@@ -2,9 +2,11 @@ package general;
 
 import java.awt.Color;
 
+import score.SerializationException;
+
 public class ColorsParser {
 
-    public Color colorFromString(String line){
+    public Color colorFromString(String line) throws SerializationException{
         Color color;
         if(line.startsWith("color(RGB)"))
         {
@@ -18,11 +20,42 @@ public class ColorsParser {
         }
         String cutLine = line.substring(6);
         String colorLine = cutLine.substring(0, cutLine.length() - 1);  
-        try {
-            java.lang.reflect.Field field = Color.class.getField(colorLine);
-            color = (Color)field.get(null);
-        } catch (Exception e) {
-            color = null; 
+        switch (colorLine) {
+        case "black":
+            color = Color.black;
+            break;
+        case "blue":
+            color = Color.blue;
+            break;
+        case "cyan":
+            color = Color.cyan;
+            break;     
+        case "gray":
+            color = Color.gray;
+            break;  
+        case "lightGray":
+            color = Color.lightGray;
+            break;
+        case "green":
+            color = Color.green;
+            break;
+        case "orange":
+            color = Color.orange;
+            break;
+        case "pink":
+            color = Color.pink;
+            break;
+        case "red":
+            color = Color.red;
+            break;
+        case "white":
+            color = Color.white;
+            break;
+        case "yellow":
+            color = Color.yellow;
+            break;
+        default:
+            throw new SerializationException(" Color not exist.");
         }
         return color;
     }
