7bf4ffb16273294ba574b042699ca80b554c7821
adielcahana <adielcahana@users.noreply.github.com>
2016-06-22 17:53:20 +0300

    Add files via upload

--- ass7/gameobjects/Enemy.java
+++ ass7/gameobjects/Enemy.java
@@ -1,11 +1,13 @@
 package gameobjects;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 
 import animations.GameLevel;
 import biuoop.DrawSurface;
+import general.GameEnvironment;
 import geometry.Point;
 import geometry.Rectangle;
 import listeners.Counter;
@@ -14,6 +16,7 @@ import listeners.HitListener;
 public class Enemy implements Collidable, Sprite, HitNotifier {
     private Rectangle shape;
     private GameLevel game;
+    private GameEnvironment environment;
     private BufferedImage image1;
     private BufferedImage image2;
     private long imageTiming;
@@ -22,7 +25,7 @@ public class Enemy implements Collidable, Sprite, HitNotifier {
     private Velocity velocity;
     private List<HitListener> hitListeners;
 
-    public Enemy(Rectangle shape, GameLevel game, BufferedImage image1, BufferedImage image2, Counter enemyCounter, Velocity velocity) {
+    public Enemy(Rectangle shape, GameLevel game, BufferedImage image1, BufferedImage image2, Counter enemyCounter, Velocity velocity, GameEnvironment environmet) {
         this.shape = shape;
         this.game = game;
         this.image1 = image1;
@@ -31,6 +34,7 @@ public class Enemy implements Collidable, Sprite, HitNotifier {
         this.imageNum = 1;
         this.velocity = velocity;
         this.hitListeners = new ArrayList<HitListener>();
+        this.environment = environmet;
     }
     @Override
     public void addHitListener(HitListener hl) {
@@ -48,14 +52,14 @@ public class Enemy implements Collidable, Sprite, HitNotifier {
         switch(imageNum) {
         case 1:
             d.drawImage((int) this.shape.getUpperLeft().getX(), (int) this.shape.getUpperLeft().getY(), this.image1);
-            if ( (currentTime - this.imageTiming)/1000 >= 0.1) { 
+            if ( (currentTime - this.imageTiming)/1000 >= 0.09) {
                 this.imageTiming = currentTime;
                 this.imageNum = 2;
             }
             break;
         case 2:
             d.drawImage((int) this.shape.getUpperLeft().getX(), (int) this.shape.getUpperLeft().getY(), this.image2);
-            if ( (currentTime - this.imageTiming)/1000 >= 0.1) { 
+            if ( (currentTime - this.imageTiming)/1000 >= 0.9) {
                 this.imageTiming = currentTime;
                 this.imageNum = 1;
             }
@@ -66,6 +70,7 @@ public class Enemy implements Collidable, Sprite, HitNotifier {
     @Override
     public void timePassed(double dt) {
         this.shape = new Rectangle(this.velocity.applyToPoint(this.shape.getUpperLeft(), dt), this.shape.getWidth(), this.shape.getHeight());
+        this.shoot();
     }
 
     @Override
@@ -88,10 +93,11 @@ public class Enemy implements Collidable, Sprite, HitNotifier {
     @Override
     public Velocity hit(Ball ball, Point collisionPoint, Velocity currentVelocity) {
         this.notifyHit(ball);
+        this.enemyCounter.decrease(1);
         this.removeFromGame(this.game);
         return null;
     }
-    
+
     /** Notify all the listeners that about the event.
      * <p>
      * @param hitter - the ball that hit the block. */
@@ -102,5 +108,17 @@ public class Enemy implements Collidable, Sprite, HitNotifier {
         for (HitListener hl : listeners) {
             hl.hitEvent(this, hitter);
         }
+    }
+    
+    public void shoot() {
+        Ball shot = new Ball((int) (this.shape.getUpperLeft().getX() + this.shape.getWidth() / 2),
+                (int) (this.shape.getUpperLeft().getY() + this.shape.getHeight() + 1), 3, Color.RED);
+        shot.setVelocity(Velocity.fromAngleAndSpeed(180, 400));
+        shot.setGameEnvironment(this.environment);
+        shot.addToGame(this.game);
+    }
+    
+    public Point getLocation() {
+        return this.shape.getUpperLeft();
     }
 }
